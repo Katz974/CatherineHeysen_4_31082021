16141edaa13f046a7027834769ec70279d826ccd
Catherine Heysen <cath.heysen@gmail.com>
2021-12-09 23:19:18 +0300

    Feat(modal form) Close the modal #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,33 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById('myTopnav')
+  if (x.className === 'topnav') {
+    x.className += ' responsive'
   } else {
-    x.className = "topnav";
+    x.className = 'topnav'
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground')
+const modalBtn = document.querySelectorAll('.modal-btn')
+const formData = document.querySelectorAll('.formData')
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach(btn => btn.addEventListener('click', launchModal))
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = 'block'
 }
 
+// close modal form
+function closeModal(event) {
+  event.preventDefault()
+  event.stopPropagation()
+  modalbg.style.display = 'none'
+}
 
+const modalCloseButton = modalbg.querySelectorAll('.close')
+
+// close modal event
+modalCloseButton.forEach(btn => btn.addEventListener('click', closeModal))
